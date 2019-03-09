e7f80e863160bfbd90248242d5702952c39c13da
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-09 22:30:16 +0100

    Portapps 1.20.2

--- main.go
+++ main.go
@@ -7,29 +7,47 @@ import (
 	"os"
 
 	. "github.com/portapps/portapps"
+	"github.com/portapps/portapps/pkg/registry"
+	"github.com/portapps/portapps/pkg/utl"
+)
+
+var (
+	app *App
 )
 
 func init() {
-	Papp.ID = "hlsw-portable"
-	Papp.Name = "HLSW"
-	Init()
+	var err error
+
+	// Init app
+	if app, err = New("hlsw-portable", "HLSW"); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
 }
 
 func main() {
-	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = CreateFolder(AppPathJoin("data"))
-	Papp.Process = PathJoin(Papp.AppPath, "hlsw.exe")
-	Papp.Args = []string{fmt.Sprintf("-PATH:%s", Papp.AppPath), fmt.Sprintf("-DATADIR:%s", Papp.DataPath)}
-	Papp.WorkingDir = Papp.AppPath
-
-	regsPath := CreateFolder(PathJoin(Papp.Path, "reg"))
-	regKey := RegExportImport{
-		Key:  `HKCU\Software\HLSW`,
-		Arch: "32",
-		File: PathJoin(regsPath, "HLSW.reg"),
+	utl.CreateFolder(app.DataPath)
+	app.Process = utl.PathJoin(app.AppPath, "hlsw.exe")
+	app.Args = []string{
+		fmt.Sprintf("-PATH:%s", app.AppPath),
+		fmt.Sprintf("-DATADIR:%s", app.DataPath),
 	}
 
-	ImportRegKey(regKey)
-	Launch(os.Args[1:])
-	ExportRegKey(regKey)
+	regsPath := utl.CreateFolder(app.RootPath, "reg")
+	regKey := registry.ExportImport{
+		Key:  `HKCU\Software\HLSW`,
+		Arch: "32",
+		File: utl.PathJoin(regsPath, "HLSW.reg"),
+	}
+
+	if err := registry.ImportKey(regKey); err != nil {
+		Log.Error().Err(err).Msg("Cannot import registry key")
+	}
+
+	defer func() {
+		if err := registry.ExportKey(regKey); err != nil {
+			Log.Error().Err(err).Msg("Cannot export registry key")
+		}
+	}()
+
+	app.Launch(os.Args[1:])
 }
