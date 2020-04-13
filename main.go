8dedad74916ef6f419935c5b483376bf9dbc3906
CrazyMax <crazy-max@users.noreply.github.com>
2020-04-13 03:34:09 +0200

    Portapps 2.1.2

--- main.go
+++ main.go
@@ -6,21 +6,22 @@ import (
 	"fmt"
 	"os"
 
-	. "github.com/portapps/portapps"
-	"github.com/portapps/portapps/pkg/registry"
-	"github.com/portapps/portapps/pkg/utl"
+	"github.com/portapps/portapps/v2"
+	"github.com/portapps/portapps/v2/pkg/log"
+	"github.com/portapps/portapps/v2/pkg/registry"
+	"github.com/portapps/portapps/v2/pkg/utl"
 )
 
 var (
-	app *App
+	app *portapps.App
 )
 
 func init() {
 	var err error
 
 	// Init app
-	if app, err = New("hlsw-portable", "HLSW"); err != nil {
-		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	if app, err = portapps.New("hlsw-portable", "HLSW"); err != nil {
+		log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
 
@@ -33,19 +34,19 @@ func main() {
 	}
 
 	regsPath := utl.CreateFolder(app.RootPath, "reg")
-	regKey := registry.ExportImport{
+	regFile := utl.PathJoin(regsPath, "HLSW.reg")
+	regKey := registry.Key{
 		Key:  `HKCU\Software\HLSW`,
 		Arch: "32",
-		File: utl.PathJoin(regsPath, "HLSW.reg"),
 	}
 
-	if err := registry.ImportKey(regKey); err != nil {
-		Log.Error().Err(err).Msg("Cannot import registry key")
+	if err := registry.Import(regKey, regFile); err != nil {
+		log.Error().Err(err).Msg("Cannot import registry key")
 	}
 
 	defer func() {
-		if err := registry.ExportKey(regKey); err != nil {
-			Log.Error().Err(err).Msg("Cannot export registry key")
+		if err := registry.Export(regKey, regFile); err != nil {
+			log.Error().Err(err).Msg("Cannot export registry key")
 		}
 	}()
 
